ba5cfa3dd3dc5c8c7805e57bad78a1c23ff7f3fe
LMENOU <lucas.menou@ynov.com>
2022-06-14 15:59:59 +0200

    new_page_and_improve_header

--- main.js
+++ main.js
@@ -35,3 +35,12 @@ function showContainer(){
     var container = document.getElementById("container");
     container.classList.remove("hide");
 }
+
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+        $('#profileImage').attr('src',
+            window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+
+
