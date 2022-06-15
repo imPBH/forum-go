c0ecb3e2f85afa22033f7018b2a12cf5c69bf12e
LMENOU <lucas.menou@ynov.com>
2022-06-15 16:15:31 +0200

    last-of-last-commit

---
+++ public/JS/main.js
@@ -0,0 +1,17 @@
+
+
+//Comment
+function showComment(){
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.remove("hide");
+}
+
+//Reply
+function showReply(){
+    var replyArea = document.getElementById("reply-area");
+    replyArea.classList.remove("hide");
+}
+
+
+
+
